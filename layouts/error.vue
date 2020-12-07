2dc8c9966b38db08d43a11aeaecd1292b9d47de1
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2020-12-07 19:00:29 +0100

    Add basic error handling.

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="nuxt-error">
+      <h2>Error</h2>
+      <h4><strong>Error:</strong> {{ error.message }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtError',
+  layout: 'default',
+  created: () => {
+    console.log(this)
+  },
+  props: {
+    error: {
+      type: Object,
+      default: () => {
+      }
+    }
+  }
+}
+</script>
